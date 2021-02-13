21298fcaad558e67996d7de559f1a3e0acce9532
Ebinu-s <ebinusuneer2nd@gmail.com>
2021-02-13 22:36:14 +0530

    added complete option

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const addbtn = document.getElementById('addBtn');
+const containerEL = document.querySelector('.container');
+const completedEL = document.querySelector('.completed');
+
 
 const textValue = JSON.parse(localStorage.getItem('notes'));
 
-var indexNoValue = 0;
+var indexNoValue = 0;   
 
 if(textValue){
 
@@ -18,16 +21,18 @@ addbtn.addEventListener('click', () => {
     addNote();
 })
 
-function addNote(text = '') { 
+function addNote(text = '') {
 
+    let isCompleted = false;
     const note = document.createElement('div'); // eee notinnote
     note.classList.add('note');
     indexNoValue++;
     note.innerHTML = `<div id="indexNo">0</div>
                         <div class="notes">
                         <div class="tools">
-                            <button title="Edit" class="editBtn"> <i class="fas fa-edit"></i> </button>
-                            <button title="Remove" class="removeBtn"> <i class="fas fa-minus-square"></i> </button>
+                            <div class='completed'><button title="completed" class="completedBtn"> <i class="far fa-check-square"></i> </button></div>
+                            <div><button title="Edit" class="editBtn"> <i class="fas fa-edit"></i> </button>
+                            <button title="Remove" class="removeBtn"> <i class="fas fa-minus-square"></i> </button></div>
                         </div>
                         <div class="main ${text? '' : 'hidden'} "></div>
                         <textarea class="${text? 'hidden' : ''}" ></textarea> <!-- TIL space between textarea can cause unneccessary space in the begining -->
@@ -39,6 +44,7 @@ function addNote(text = '') {
 
     const editBtn = note.querySelector('.editBtn');
     const removeBtn = note.querySelector('.removeBtn');
+    const completeBtn = note.querySelector('.completedBtn');
     const toolsBar = note.querySelector('.tools');
 
     const indexNumEl = note.querySelector('#indexNo');
@@ -55,7 +61,6 @@ function addNote(text = '') {
     removeBtn.addEventListener('click', ()=>{
         note.remove();
         updateLs();
-
     });
 
     textArea.addEventListener('input', (e) =>{
@@ -79,14 +84,44 @@ function addNote(text = '') {
     editBtn.addEventListener('mouseover', () => {
         toolsBar.style.background = '#FFBA49';
         indexNumEl.style.background = '#FFBA49';
-    })
+    });
+    
+    completeBtn.addEventListener('mouseover', ()=>{
+        toolsBar.style.background = '#a1cae2';
+        indexNumEl.style.background = '#a1cae2';
+    });
 
     toolsBar.addEventListener('mouseout', ()=> {
         toolsBar.style.background = '#78e08f';
         indexNumEl.style.background = '#78e08f';
-    })
+    });
+    
+    completeBtn.addEventListener('click', ()=>{
+        if(isCompleted){
+            //toglle back
+            isCompleted = false;
+            editBtn.style.pointerEvents = 'auto';
+            textArea.disabled = false;
+            textArea.style.color = '#010101';
+            main.style.color = '#010101';
+            editBtn.style.opacity = '1';
+            main.style.textDecoration= 'none';
+        }
+        else{
+            isCompleted = true;
+            editBtn.style.pointerEvents = 'none';
+            textArea.style.color = '#317b56';
+            editBtn.style.opacity = '0.2';
+            textArea.disabled = true;
+            main.style.color = '#317b56 !important';
+            toolsBar.style.background = '#a1cae2 !important';  // fix color not working
+            indexNumEl.style.background = '#a1cae2';
+            main.style.textDecoration= 'line-through';
+        }
+        updateLs();
+    });
 
-    document.body.appendChild(note); 
+    containerEL.appendChild(note); 
 
     updateLs();
 
